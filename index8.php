cdfbbaa063f73edb73837c9537e2bf881d308a6a
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2013-04-01 19:41:19 +0700

    Write index 8 new

--- index8.php
+++ index8.php
@@ -4,7 +4,7 @@
 	$dbhost="localhost";	
 	$dbuser='root';			
 	$dbpass='1234';			
-	$dbname='nobility_clean';			
+	$dbname='nobility';			
 
 	global $conn; 
 	$conn=mysql_connect($dbhost,$dbuser,$dbpass) or die ("ไม่สามารถเชื่อมต่อโฮสต์ได้");
@@ -15,10 +15,10 @@
 
 	//Start Query Data Here
 	$sql = sprintf("SELECT
-				personel.personel_id,
-				personel.image
+				personel_heir.personel_id,
+				personel_heir.heir_id
 				FROM
-				personel
+				personel_heir
 			");
 	
 	$result = mysql_query($sql);
@@ -30,7 +30,7 @@
 		echo "<tr>";
 		
 		echo "<td>".$row['personel_id']."</td>";
-		echo "<td>".$row['image']."</td>";
+		echo "<td>".$row['heir_id']."</td>";
 		
 		/*
 		if(preg_match("/^Thumbnails_(.+)$/",$row['image'],$match))
@@ -38,6 +38,21 @@
 			echo "<td>".$match[1]."</td>";
 			$sql2 = sprintf("UPDATE personel SET personel.image = '%s' WHERE personel.personel_id = '%s'",$match[1],$row['personel_id']);
 			mysql_query($sql2);
+		}
+		*/
+		
+		$sql2 = sprintf("UPDATE heir SET heir.personel_id = '%s' WHERE heir.heir_id = '%s'",$row['personel_id'],$row['heir_id']);
+		
+		/*
+		if(mysql_query($sql2))
+		{
+		
+			$sql3 = sprintf("DELETE FROM personel_heir WHERE personel_heir.personel_id = '%s' AND personel_heir.heir_id = '%s'",$row['personel_id'],$row['heir_id']);
+			
+			if(mysql_query($sql3))
+			{
+				echo "<td style='color:green'>OK!!</td>";
+			}
 		}
 		*/
 		
